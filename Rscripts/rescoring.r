af51bebb7bb7eaf80e734b8b0b2fe23cc6352dd1
Christopher2212 <chrisgx21@gmail.com>
2022-10-12 11:16:28 -0700

    Little Tweaks

--- Rscripts/rescoring.r
+++ Rscripts/rescoring.r
@@ -26,10 +26,10 @@ session_results_df <- data.frame(session_id <- integer(), theta <- double())
         x=vector()
         tryCatch({
             rm(params)
-        }, error = function(err){}
+        }, error <- function(err){}
         )
-        params = matrix(,nrow =0 , ncol = 4)
-        dimnames(params) = list(c(),c("a","b","c","d"))
+        params <- matrix(,nrow =0 , ncol = 4)
+        dimnames(params) <- list(c(),c("a","b","c","d"))
         
         #TODO: fix this loop length as well
         #iterate through all the answers in a given session
@@ -38,25 +38,27 @@ session_results_df <- data.frame(session_id <- integer(), theta <- double())
             tryCatch({
                 score <- list_score_itemid_from_session[i,1]
                 question_id <- list_score_itemid_from_session[i,2]
-                # print(question_id)
-                # print(score)
+
+                print(question_id)
+                print(score)
+
                 p1 <- 2
                 p2 <- 0
                 p3 <- 0.25
                 p4 <- 1
-                x = append(x,score)
+                x <- append(x,score)
                 params <- rbind(params,c(p1,p2,p3,p4))
             }, error = function(err){}
             )
         }
 
         #calculating the new theta, but I don't know why this check is here
-        if(length(x) > 35){
+        if(length(x) > 5){
 
-            new_theta = thetaEst(params,x)
+            new_theta <- thetaEst(params,x)
             # print(new_theta)
-            session_id = desired_session_id
-            theta = new_theta
+            session_id <- list_unique_sessions[s,]
+            theta <- new_theta
             session_data <- data.frame(session_id,theta)
             session_results_df <- rbind(session_results_df,session_data)
         }
@@ -66,10 +68,11 @@ session_results_df <- data.frame(session_id <- integer(), theta <- double())
     for(k in 1:40){
 
         results = list()
-        r=0
 
         #TODO: I will need to remake this query so that it works per tabl
         #grabbing the entire list of sessions 
+
+        #the bottom % of the thetas grab the score of what they got
 
         #grabbing the entire list of question #s
         #grabbing the entire list of scores
@@ -79,16 +82,21 @@ session_results_df <- data.frame(session_id <- integer(), theta <- double())
 
         for(b in 1:nrow(list_sessionid_itemid_score_from_table)){
             session_id <- list_sessionid_itemid_score_from_table[b,1]
+            print(session_id)
             question_number <- list_sessionid_itemid_score_from_table[b,2]
-
-            #Why this is here I have no idea
-            if(question_number == 4001){
-                r = r+1
-                print(r)
-            }
+            print(question_number)
 
             result <- list_sessionid_itemid_score_from_table[b,3] 
-            
+
+            #remake 
+            if(session_id%in%session_results_df$session_id){
+                theta = as.numeric(session_results_df$theta[[match(session_id,session_results_df$session_id)]])
+
+                if(length(results)<question_number){
+                    results[[question_number]] = list()
+                }
+                results[[question_number]][[length(results[[question_number]])+1]]=list(theta,result)
+            }
         }
     }
 
